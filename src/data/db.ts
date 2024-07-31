3dd5fc5554a550fee0280650e9d5a332196b2695
Pablo <padoro89@gmail.com>
2024-07-31 16:13:09 +0200

    primer commit

---
+++ src/data/db.ts
@@ -0,0 +1,64 @@
+import type { MenuItem } from '../types'
+
+export const menuItems: MenuItem[] = [
+  {
+    "id": 1,
+    "name": "Pizza a la Leña Chica",
+    "price": 9.99
+  },
+  {
+    "id": 2,
+    "name": "Pizza a la Leña Mediana",
+    "price": 13.99
+  },
+  {
+    "id": 3,
+    "name": "Rebanada de Pay de Limón",
+    "price": 13.99
+  },
+  {
+    "id": 4,
+    "name": "Rebanada de Pastel de Chocolate",
+    "price": 13.99
+  },
+  {
+    "id": 5,
+    "name": "Jugo de Naranja",
+    "price": 9.99
+  },
+  {
+    "id": 6,
+    "name": "Pizza a la Leña Grande",
+    "price": 19.99
+  },
+  {
+    "id": 7,
+    "name": "Rib Eye 800g",
+    "price": 24.99
+  },
+  {
+    "id": 8,
+    "name": "Jugo de Naranja",
+    "price": 9.99
+  },
+  {
+    "id": 9,
+    "name": "Tequila",
+    "price": 15.99
+  },
+  {
+    "id": 10,
+    "name": "Rebanada de Pay de Queso",
+    "price": 13.99
+  },
+  {
+    "id": 11,
+    "name": "Café Americano",
+    "price": 11.99
+  },
+  {
+    "id": 12,
+    "name": "Café Capuchino",
+    "price": 15.99
+  }
+]
